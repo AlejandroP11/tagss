81eb1a747395239ec571b89faaae002a427ecc26
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2021-11-17 14:25:02 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nombreP= JOptionPane.showInputDialog("Introduce tu nombre");
+        float ventasA=Integer.parseInt(JOptionPane.showInputDialog("Introduce las ventas anuales"));
+        if (ventasA<=100)
+            System.out.println(nombreP+" es un producto de bajo consumo");
+        else
+        if(ventasA<=500)
+            System.out.println(nombreP+" es un producto de consumo medio");
+        else
+        if(ventasA<=1000)
+            System.out.println(nombreP+" es un producto de alto consumo");
+        else
+            System.out.println(nombreP+" es un producto de primera necesidad");
+    }
+    }
+
