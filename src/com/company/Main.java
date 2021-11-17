a1a843356d2262db710e96648e4e6b3937269bb0
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2021-11-17 14:33:08 +0100

    si

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        String nombreP= JOptionPane.showInputDialog("Introduce tu nombre");
+        String nombreP= JOptionPane.showInputDialog("Introduce el nombre del producto");
         float ventasA=Integer.parseInt(JOptionPane.showInputDialog("Introduce las ventas anuales"));
         if (ventasA<=100)
             System.out.println(nombreP+" es un producto de bajo consumo");
